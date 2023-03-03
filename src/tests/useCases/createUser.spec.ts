2a5b816250194d3ae4bc896de1fb720a4d0b2ac2
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2023-03-03 17:19:05 -0300

    #2 refactor data inputs of createUserUseCase tests

--- src/tests/useCases/createUser.spec.ts
+++ src/tests/useCases/createUser.spec.ts
@@ -31,12 +31,33 @@ const makeUserRepository = (): IUserRepository => {
                 name: 'any_name',
                 email: 'any_email@email.com',
                 price_hour: 10.5,
+                createdAt: new Date(),
+                updatedAt: new Date(),
             }
             return new Promise((resolve) => resolve(fake))
         }
     }
     return new UserRepositoryStub()
 }
+
+const dataInput = (): UserSchema[] => [
+    {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        price_hour: 10.5,
+        password: 'any_password',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        price_hour: 10.5,
+        password: 'hash_password',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+]
 
 const makeSut = (): sutTypes => {
     const userRepositoryStub = makeUserRepository()
@@ -49,19 +70,13 @@ const makeSut = (): sutTypes => {
 describe('test create user use case', () => {
     test('test call getUserByEmail method to verify user exist', async () => {
         const { sut, userRepositoryStub } = makeSut()
+
         const userRepositoryStubSpy = vi.spyOn(
             userRepositoryStub,
             'getUserByEmail'
         )
-        const data = {
-            name: 'any_name',
-            email: 'any_email@email.com',
-            price_hour: 10.5,
-            password: 'any_password',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        }
-        await sut.execute(data)
+
+        await sut.execute(dataInput()[0])
         expect(userRepositoryStubSpy).toHaveBeenCalledWith(
             'any_email@email.com'
         )
@@ -69,6 +84,7 @@ describe('test create user use case', () => {
 
     test('test return error if user exist', async () => {
         const { sut, userRepositoryStub } = makeSut()
+
         vi.spyOn(userRepositoryStub, 'getUserByEmail').mockReturnValue(
             new Promise((resolve) =>
                 resolve({
@@ -81,15 +97,8 @@ describe('test create user use case', () => {
                 })
             )
         )
-        const data = {
-            name: 'any_name',
-            email: 'any_email@email.com',
-            price_hour: 10.5,
-            password: 'any_password',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        }
-        const userExists = await sut.execute(data)
+
+        const userExists = await sut.execute(dataInput()[0])
         expect(userExists).toEqual(
             new Error('Account with this email already exists!')
         )
@@ -97,50 +106,33 @@ describe('test create user use case', () => {
 
     test('test call hash password method', async () => {
         const { sut, encrypterStub } = makeSut()
+
         const encrypterSpy = vi.spyOn(encrypterStub, 'hash')
-        const data = {
-            name: 'any_name',
-            email: 'any_email@email.com',
-            price_hour: 10.5,
-            password: 'any_password',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        }
-        await sut.execute(data)
+
+        await sut.execute(dataInput()[0])
         expect(encrypterSpy).toHaveBeenCalledWith('any_password')
     })
 
     test('test call create method of user repository', async () => {
         const { sut, userRepositoryStub } = makeSut()
+
         const userRepositoryStubSpy = vi.spyOn(userRepositoryStub, 'create')
-        const data = {
-            name: 'any_name',
-            email: 'any_email@email.com',
-            price_hour: 10.5,
-            password: 'hash_password',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        }
-        await sut.execute(data)
-        expect(userRepositoryStubSpy).toHaveBeenCalledWith(data)
+
+        await sut.execute(dataInput()[1])
+        expect(userRepositoryStubSpy).toHaveBeenCalledWith(dataInput()[1])
     })
 
     test('test return success create user', async () => {
         const { sut } = makeSut()
-        const data = {
-            name: 'any_name',
-            email: 'any_email@email.com',
-            price_hour: 10.5,
-            password: 'hash_password',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        }
-        const user = await sut.execute(data)
+
+        const user = await sut.execute(dataInput()[1])
         expect(user).toEqual({
             id: 1,
             name: 'any_name',
             email: 'any_email@email.com',
             price_hour: 10.5,
+            createdAt: new Date(),
+            updatedAt: new Date(),
         })
     })
 })
