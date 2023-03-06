37f041b9d993e78e45985e10e6fe2dd7e447d551
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2023-03-06 15:26:06 -0300

    #2 rename directory models to types

---
+++ src/types/user.ts
@@ -0,0 +1,20 @@
+type UserSchema = {
+    name: string
+    email: string
+    password: string
+    price_hour: number
+    createdAt?: Date | null
+    updatedAt?: Date | null
+}
+
+type CreatedUser = {
+    id: number
+    name: string
+    email: string
+    password?: string
+    price_hour: number
+    createdAt?: Date | null
+    updatedAt?: Date | null
+}
+
+export { UserSchema, CreatedUser }
