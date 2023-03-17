9f9fe327b34f8318f88afe310af1fc0be30fc815
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2023-03-17 15:44:34 -0300

    set IGetUserEmailREpository on signup factorie

--- src/config/factories/signup.ts
+++ src/config/factories/signup.ts
@@ -1,6 +1,8 @@
 import { SignupController } from '../../controllers/user/signupController'
 import { clientdB } from '../../database/client'
 import { Encrypter } from '../../helpers/encrypter/encrypterAdapter'
+import { IAddUserRepository } from '../../repositories/user/intefaces/IAddUserRepository'
+import { IGetUserEmailRepository } from '../../repositories/user/intefaces/IGetUserEmailRepository'
 import { UserRepository } from '../../repositories/user/userRepository'
 import { CreateUserUseCase } from '../../useCases/user/createUser/createUser'
 
@@ -8,7 +10,11 @@ const makeSignupController = (): SignupController => {
     const salt = 12
     const userRepository = new UserRepository(clientdB)
     const encrypter = new Encrypter(salt)
-    const createUserUseCase = new CreateUserUseCase(userRepository, encrypter)
+    const createUserUseCase = new CreateUserUseCase(
+        userRepository as IAddUserRepository,
+        userRepository as IGetUserEmailRepository,
+        encrypter
+    )
     return new SignupController(createUserUseCase)
 }
 
