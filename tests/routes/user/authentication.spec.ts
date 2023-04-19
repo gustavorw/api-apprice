8ceca434af4b6aaaffe604c3df59dbaf18088525
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2023-04-19 14:31:36 -0300

    #4 return ok if token privided

--- tests/routes/user/authentication.spec.ts
+++ tests/routes/user/authentication.spec.ts
@@ -2,15 +2,37 @@ import { afterAll, describe, test } from 'vitest'
 import resquest from 'supertest'
 import { app } from '../../../src/config/app'
 import { clientdB } from '../../../src/database/client'
+import jwt from 'jsonwebtoken'
+import env from '../../../src/config/env'
 
 const client = clientdB
+
+const data = {
+    name: 'teste',
+    email: 'test@email.com',
+    password: 'test1234',
+    createdAt: new Date('2023-03-08T09:00'),
+    updatedAt: new Date('2023-03-08T09:00'),
+}
 
 describe('test private router', () => {
     afterAll(async () => {
         await client.user.deleteMany({})
     })
 
-    test('test error unauthorized', async () => {
+    test('test return unauthorized', async () => {
         await resquest(app).get('/api/v1/user/profile').expect(401)
+    })
+
+    test('test return ok', async () => {
+        const user = await client.user.create({ data })
+        const token = jwt.sign({ id: user.id }, env.secret_key, {
+            expiresIn: '8h',
+        })
+
+        await resquest(app)
+            .get('/api/v1/user/profile')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
     })
 })
